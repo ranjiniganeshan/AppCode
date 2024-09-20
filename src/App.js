a54cec12a492e47db42683ac4d26a40c778fc57a
ranjiniganeshan <ranjiniganeshan@gmail.com>
2024-09-20 17:21:38 +0530

    added code

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    10WeeksofCloudOps - TODO LIST
+                    ranjini - TODO LIST
                 </Row>
   
                 <hr />
